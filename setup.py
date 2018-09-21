085ba981c85e198bff128131cc56de520d1f8276
Muneeb <muneeb.gandapur@gmail.com>
2018-09-21 09:25:58 +0500

    Fixed import errors

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+import setuptools
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
-setup(name='Zizou',
+setup(name='zizou',
 	version='1.0',
 	description='A package dedicated to the G.O.A.T, Zizou. Zidane means to grow and help others grow.',
 	long_description=readme(),
@@ -12,7 +13,10 @@ setup(name='Zizou',
 	author='Muneeb Ahmad',
 	author_email='muneeb.gandapur@gmail.com',
 	#entry_points = {
-	#	'console_scripts': ['Zizou=Zizou.main:main']
+	#	'console_scripts': ['zizou=Zizou.main:main']
 	#},
 	packages=setuptools.find_packages(),
+	install_requires=[
+		"ipaddress"
+	],
 	zip_safe=False)
---
+++ zizou/__init__.py
@@ -0,0 +1,1 @@
+from src.zizou import Zizou
